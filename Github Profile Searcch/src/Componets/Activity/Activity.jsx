33d7bf605995d7a3e5ee253c0ca9393ace93d081
rachit <curiousrachit.26@gmail.com>
2023-08-12 22:23:06 +0530

    repo Section completed

--- Github Profile Searcch/src/Componets/Activity/Activity.jsx
+++ Github Profile Searcch/src/Componets/Activity/Activity.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import "./Activity.css";
+function Activity({ Activity }) {
+    return (
+        <>
+            {
+                Activity?.map((acti, i) => (
+                    <div key={id} className='acti-data'>
+                        <img src={acti.actor?.avatar_url}></img>
+                        <h1>
+                            {acti?.actor?.login}
+                            {acti?.type}
+                            <br />
+                            {acti?.repo?.name}
+                        </h1>
+                    </div>
 
-function Activity() {
-  return (
-    <div>Activity</div>
-  )
+                ))
+            }
+
+        </>
+    )
 }
 
 export default Activity
--- Github Profile Searcch/src/Componets/Repo/Repo.jsx
+++ Github Profile Searcch/src/Componets/Repo/Repo.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import "./Repo.css";
+function Repo({ users }) {
+    return (
+        <>
+            {
+                users && users.map((user, id) => (
 
-function Repo() {
-  return (
-    <div>Repo</div>
-  )
+                    <div key={id} className='repo-box'  >
+                        <a href={user.html_url} target='_blank'>
+                            {user.full_name}
+
+                        </a>
+
+                        <div className="user-foks">
+                        <h1> Language:{user.language}</h1>
+                          
+                            <h1 className='fork'> Fork:{user.forks}</h1>
+                            <h1 className='star'>Sarts:{user.stargazers_count}</h1>
+
+                        </div>
+
+                    </div>
+
+                ))}
+
+        </>
+    )
 }
 
 export default Repo
